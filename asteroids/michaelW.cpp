f4260c4545c4b3a19537dfc300d2496901118eb9
mwines <mwines92@gmail.com>
2015-06-03 19:57:28 -0700

    Update michaelW.cpp

--- asteroids/michaelW.cpp
+++ asteroids/michaelW.cpp
@@ -19,13 +19,20 @@ void setShipTexture(Game *g)  {
     glBindTexture(GL_TEXTURE_2D, shipTexture);
     glRotatef(g->ship.angle, 0.0f, 0.0f, 1.0f);
     glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(  -20.0f,-10.0f); glTexCoord2f(0.5f, 0.2f);
+    glVertex2f(  -20.0f,-10.0f); glTexCoord2f(1.0f, 0.0f);
     glVertex2f(0.0f,  20.0f); glTexCoord2f(0.5f, 1.0f);
-    glVertex2f(0.0f,  -6.0f);  glTexCoord2f(0.0f, 0.2f);
-    glVertex2f(20.0f,  -10.0f); glTexCoord2f(1.0f, 0.2f);
+    glVertex2f(0.0f,  -6.0f);  glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(20.0f,  -10.0f); glTexCoord2f(0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glEnd();
 }
-
+/*
+void changeShipTexture(void)  {
+    shipImage = ppm6GetImage((char*)"./images/ship2.ppm");
+    glGenTextures(1, &shipTexture);
+    glBindTexture(GL_TEXTURE_2D, shipTexture);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, shipImage->width, shipImage->height, GL_RGB, GL_UNSIGNED_BYTE, shipImage->data);
+}
+*/
 
 
